4c91645157a2ba93e50c329f582548a861e01000
James McDougall <james.mcdougall@goldkey.com>
2021-07-26 08:46:51 -0500

    Initial project Setup

---
+++ Apple Frameworks/Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by James McDougall on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
