2f8be877dc9c11487871197ff5ab300a4bbffc39
james-mcdougall <jameswilliammcdougall@gmail.com>
2021-08-17 20:13:54 -0500

    Fixed errors, now initial commit

---
+++ AppleFrameworks - SwiftUI/AppleFrameworks - SwiftUI/AppleFrameworks_SwiftUI.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUI.swift
+//  AppleFrameworks - SwiftUI
+//
+//  Created by James McDougall on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
